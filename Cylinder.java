cfacadae4455459e08bdf1e991b884d23b93781d
tminhky <125253696+tminhky@users.noreply.github.com>
2023-03-04 12:19:44 +0700

    Create Cylinder.java

---
+++ Cylinder.java
@@ -0,0 +1,22 @@
+package Lab4;
+
+public class Cylinder extends Circle {
+    private double heigth;
+
+    public Cylinder(double radius, String color, double heigth) {
+        super(radius, color);
+        this.heigth = heigth;
+    }
+
+    public double getHeigth() {
+        return heigth;
+    }
+
+    public void setHeigth(double heigth) {
+        this.heigth = heigth;
+    }
+
+    public double getVolume() {
+        return super.getArea() * this.heigth;
+    }
+}
